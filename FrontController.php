f6bd2a221ab0a9d9f5d5fa0591d735621f932413
diosmosis <benaka@piwik.pro>
2015-06-24 01:21:11 -0700

    More debugging.

--- FrontController.php
+++ FrontController.php
@@ -251,7 +251,7 @@ class FrontController extends Singleton
         // try to get an option (to check if data can be queried)
         try {
             Option::get('TestingIfDatabaseConnectionWorked');
-        } catch (Exception $exception) {echo "<pre>".get_class(Config::getInstance())."</pre>";
+        } catch (Exception $exception) {echo "<pre>".Config::getInstance()->getLocalPath()."</pre>";
             if (self::shouldRethrowException()) {
                 throw $exception;
             }
